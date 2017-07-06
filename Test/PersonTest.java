9b16421387a5a8381173729cb4dfed0346089165
Suwadith <suwadith06@gmail.com>
2017-07-06 09:39:12 +0530

    Initial commit

---
+++ Test/PersonTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Suwadith on 7/6/2017.
+ */
+public class PersonTest {
+
+    private Person jack = new Person("Jack", 21, "Male");
+
+    @Test
+    public void getName() throws Exception {
+        assertEquals("Jack", jack.getName());
+    }
+
+    @Test
+    public void getAge() throws Exception {
+        assertEquals(21, jack.getAge());
+    }
+
+    @Test
+    public void getGender() throws Exception {
+        assertEquals("Male", jack.getGender());
+    }
+
+    @Test
+    public void testHappyAtStart() throws Exception {
+        assertFalse(jack.isHappy());
+    }
+
+    @Test
+    public void testHappyAfterPlay() throws Exception {
+    jack.playWithRock();
+        assertTrue(jack.isHappy());
+    }
+
+    
+}
---
+++ src/Person.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Suwadith on 7/6/2017.
+ */
+public class Person {
+
+    private String name;
+    private int age;
+    private String gender;
+    private boolean happy = false;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public boolean isHappy() {
+        return happy;
+    }
+
+    public void playWithRock() {
+        happy = true;
+    }
+}
